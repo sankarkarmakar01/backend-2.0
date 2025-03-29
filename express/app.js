146d80d6700042a49bee7c9564343d682e6d9e29
sankarkarmakar01 <karmakarsankar000@gmail.com>
2025-03-29 08:54:06 +0530

    Commit 29-03-2025

--- express/app.js
+++ express/app.js
@@ -3,20 +3,38 @@ import { PORT } from "./env.js";
 import path from "path";
 
 const app = express();
-const staticPath = path.join(import.meta.dirname,"public");
-app.use("/public",express.static(staticPath));
-// app.use(express.static(staticPath));
+const staticPath = path.join(import.meta.dirname, "public");
+app.use("/public", express.static(staticPath));
 
-app.get("/", (req, res) => {
-  // console.log(__dirname);
-  // console.log(__filename)
-  // console.log(import.meta.url)
-  // const __filename = new URL(import.meta.url).pathname
-  // console.log(__filename)
+/*
+//* app.use(express.static(staticPath));
+*app.get("/", (req, res) => {
+  //* console.log(__dirname);
+  //* console.log(__filename)
+  //* console.log(import.meta.url)
+  //* const __filename = new URL(import.meta.url).pathname
+  //* console.log(__filename)
 
-  const homePagePath = path.join(import.meta.dirname, "public", "index.html");
+  *const homePagePath = path.join(import.meta.dirname, "public");
 
-  res.sendFile(homePagePath);
+  *res.sendFile(homePagePath);
+*});
+*/
+
+app.get("/product", (req, res) => {
+  console.log(req.query);
+  res.send(`<h1>Product Page </h1>`);
+});
+
+app.get("/profile/:username", (req, res) => {
+  console.log(req.params);
+  res.send(`<h1>My username is ${req.params.username}</h1>`);
+});
+
+app.get("/profile/:username/article/:slug", (req, res) => {
+  console.log(req.params);
+  const formatedSlug = req.params.slug.replace(/-/g, " ");
+  res.send(`<h1>Article ${req.params.username} by ${formatedSlug}</h1>`);
 });
 
 app.listen(PORT, () => {
---
+++ express/projects/app.js
@@ -0,0 +1,87 @@
+import { readFile, writeFile } from "fs/promises";
+import path from "path";
+import crypto from "crypto";
+import express from "express";
+import { link } from "fs";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const DATA_FILE = path.join("data", "links.json");
+
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+const loadLinks = async () => {
+  try {
+    const data = await readFile(DATA_FILE, "utf-8");
+    return JSON.parse(data);
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      await writeFile(DATA_FILE, JSON.stringify({}));
+      return {};
+    }
+    throw error;
+  }
+};
+
+const saveLinks = async (links) => {
+  await writeFile(DATA_FILE, JSON.stringify(links));
+};
+
+app.get("/", async (req, res) => {
+  try {
+    const file = await readFile(path.join("views", "index.html"));
+    const links = await loadLinks();
+
+    const content = file.toString().replaceAll(
+      "{{ shortened_urls }}",
+      Object.entries(links)
+        .map(
+          ([shortCode, url]) =>
+            `<li><a href="/${shortCode}" target="_blank">${req.host}/${shortCode}</a> - ${url}</li>`
+        )
+        .join("")
+    );
+    return res.send(content);
+  } catch (error) {
+    return res.status(500).send("Internal server error");
+  }
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const { url, shortCode } = req.body;
+    const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex");
+
+    const links = await loadLinks();
+
+    if (links[finalShortCode]) {
+      return res
+        .status(400)
+        .send("Short code already exists. Please choose another.");
+    }
+
+    links[finalShortCode] = url;
+    await saveLinks(links);
+    return res.redirect("/");
+  } catch (error) {}
+});
+
+app.get("/:shortCode", async (req, res) => {
+  try {
+    const { shortCode } = req.params;
+    const links = await loadLinks();
+
+    if (!links[shortCode]) return res.status(404).send("404 error occured");
+
+    return res.redirect(links[shortCode]);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send("Internal server error");
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at ${PORT}`);
+});
